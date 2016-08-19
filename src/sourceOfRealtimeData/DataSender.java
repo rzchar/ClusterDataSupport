e5eecd0f55e19fbffd8c617e717141829aba6183
rzchar <qinyidan@126.com>
2016-08-19 20:55:59 +0800

    refactor the fake data maker

--- src/sourceOfRealtimeData/DataSender.java
+++ src/sourceOfRealtimeData/DataSender.java
@@ -7,28 +7,38 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DataSender {
-	private String fakeData(){
+	private String fakeData() {
+		String fakeString = "{\"memory\":\"3698.0\","
+			+ "\"networkReceive\":\"25255433896\",\"networkSend\":\"56800284169\","
+			+ "\"createTime\":\"1471560966101\",\"cpu\":\"0.629\"}";
 		Random r = new Random();
-		double cpu = r.nextDouble() * 0.3 + 0.5;
+		double cpu = r.nextDouble() * 0.2 - 0.1;
 		long l = Runtime.getRuntime().freeMemory();
-		long netsent = (1<<30)+r.nextInt(1<<28);
-		long netreceive = (1<<30)+r.nextInt(1<<28);
-		JSONObject jo = new JSONObject();
+		long netsent = (1 << 30) + r.nextInt(1 << 28);
+		long netreceive = (1 << 30) + r.nextInt(1 << 28);
 		try {
-			jo.accumulate("cpu", cpu);
+			JSONObject jojo = new JSONObject(fakeString);
+			JSONObject jo = new JSONObject();
+			jo.accumulate("cpu", jojo.getDouble("cpu") + cpu);
 			jo.accumulate("nts", netsent);
 			jo.accumulate("ntr", netreceive);
-			jo.accumulate("mem", l);
-			jo.accumulate("createtime", Calendar.getInstance().getTimeInMillis());
+			jo.accumulate("mem", jojo.getDouble("memory") + l * 0.01);
+			jo.accumulate("createtime",
+				Calendar.getInstance().getTimeInMillis());
+
+			System.out.println("backend :: " + jo.toString());
+			return jo.toString();
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		System.out.println("backend :: " + jo.toString());
-		return jo.toString();
+		return "{}";
 	}
-	
-	static public void main(String[] args){
-		DataSender ds= new DataSender();
-		System.out.println(ds.fakeData());
+
+	static public void main(String[] args) {
+		DataSender ds = new DataSender();
+		int i = 0;
+		while (i < 10) {
+			System.out.println(ds.fakeData());
+		}
 	}
 }
--- src/sourceOfRealtimeData/HttpRequester.java
+++ src/sourceOfRealtimeData/HttpRequester.java
@@ -13,30 +13,31 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
-
+import org.json.JSONObject;
 
 public class HttpRequester {
-private static final String APPLICATION_JSON = "application/json";
-    
-    private static final String CONTENT_TYPE_TEXT_JSON = "text/json";
+	private static final String APPLICATION_JSON = "application/json";
 
-    public static void httpPostWithJSON(String url, String json){
-        // 将JSON进行UTF-8编码,以便传输中文
+	private static final String CONTENT_TYPE_TEXT_JSON = "text/json";
+
+	public static void httpPostWithJSON(String url, String json) {
+		// 将JSON进行UTF-8编码,以便传输中文
 		try {
 			String encoderJson = URLEncoder.encode(json, "UTF-8");
 			CloseableHttpClient httpClient = HttpClients.createDefault();
-	        HttpPost httpPost = new HttpPost(url);
-	        httpPost.addHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON);
-	        
-	        StringEntity se = new StringEntity(encoderJson);
-	        se.setContentType(CONTENT_TYPE_TEXT_JSON);
-	        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON));
-	        httpPost.setEntity(se);
-	        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
-	        HttpEntity responseEntity =  httpResponse.getEntity();
-	        System.out.println(responseEntity);
-	        httpResponse.close();
-	        httpClient.close();
+			HttpPost httpPost = new HttpPost(url);
+			httpPost.addHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON);
+
+			StringEntity se = new StringEntity(encoderJson);
+			se.setContentType(CONTENT_TYPE_TEXT_JSON);
+			se.setContentEncoding(
+				new BasicHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON));
+			httpPost.setEntity(se);
+			CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity responseEntity = httpResponse.getEntity();
+			System.out.println(responseEntity);
+			httpResponse.close();
+			httpClient.close();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (ClientProtocolException e) {
@@ -44,7 +45,11 @@ private static final String APPLICATION_JSON = "application/json";
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        
-        
-    }
-} 
+
+	}
+	
+	static public void main(String[] args){
+		String uu = "http://localhost:8080/ClusterData/servlet/AddRealTimeData";
+		JSONObject jo = new JSONObject();
+	}
+}
