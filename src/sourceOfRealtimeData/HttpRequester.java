5a18df81167cd94c8cde25ba50a003f1d7994a00
rzchar <qinyidan@126.com>
2016-08-19 18:12:42 +0800

    let's commit(too lazy to write comment)

---
+++ src/sourceOfRealtimeData/HttpRequester.java
@@ -0,0 +1,50 @@
+package sourceOfRealtimeData;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+
+
+public class HttpRequester {
+private static final String APPLICATION_JSON = "application/json";
+    
+    private static final String CONTENT_TYPE_TEXT_JSON = "text/json";
+
+    public static void httpPostWithJSON(String url, String json){
+        // 将JSON进行UTF-8编码,以便传输中文
+		try {
+			String encoderJson = URLEncoder.encode(json, "UTF-8");
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+	        HttpPost httpPost = new HttpPost(url);
+	        httpPost.addHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON);
+	        
+	        StringEntity se = new StringEntity(encoderJson);
+	        se.setContentType(CONTENT_TYPE_TEXT_JSON);
+	        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, APPLICATION_JSON));
+	        httpPost.setEntity(se);
+	        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+	        HttpEntity responseEntity =  httpResponse.getEntity();
+	        System.out.println(responseEntity);
+	        httpResponse.close();
+	        httpClient.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+        
+    }
+} 
